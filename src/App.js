80ab51b68fda658eda2c297562665ec5c17b3ed5
NicoSher <nsher@frba.utn.edu.ar>
2023-08-31 20:57:56 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TIENDA DE NICO
         </p>
         <a
           className="App-link"
