85a1ac3b042b965aa7e564ced518de87e63a8494
NicoSher <nsher@frba.utn.edu.ar>
2023-09-12 21:32:15 -0300

    PreEntrega1-Sher

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,6 +1,6 @@
 const CartItems = ({ count = 0 }) => {
   return (
-    <span class='position-absolute translate-middle badge rounded-pill bg-primary start-100 top-0'>
+    <span class='position-absolute translate-middle badge rounded-pill bg-white start-100 top-50 ms-1 text-dark '>
       {count}
     </span>
   );
